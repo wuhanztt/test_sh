0d213ba7dafc3cf934d73497bbea9f005e7d3028
wuhanztt <50106813+wuhanztt@users.noreply.github.com>
2019-05-31 22:36:47 +0800

    增加一个函数

--- scripts/test_abc.py
+++ scripts/test_abc.py
@@ -5,7 +5,10 @@ class TestContact:
     def test_login(self):
         print("123")
         assert 1
-
+        
+    def test_login1(self):
+        print("123")
+        assert 0
 
 if __name__ == '__main__':
     pytest.main()
